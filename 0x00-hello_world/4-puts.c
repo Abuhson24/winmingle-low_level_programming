47b1d253e3a4e8a9e5b4792183586da79f8549e5
root <root@Abuhson-Dynasty.localdomain>
2026-05-08 08:21:36 +0100

    Add program that prints message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* This is my fourth task on c */
+int main(void)
+{
+	puts ("\"programming is like building a multulingual puzzled");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* i am making progress with c*/
+int main (void)
+{
+	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("Size of a int: %zu bytes(S)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
+
+	return (0);
+}
+		
